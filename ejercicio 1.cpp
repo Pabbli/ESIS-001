62aecec61a353f319a0feb5e50d1834ee6ad9c60
pabbli <pfroquem@unjbg.edu.pe>
2025-06-08 14:57:15 -0500

    creacion programa principal

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -4,9 +4,16 @@ using namespace std;
 float suma(int num1, int num2) {
 	return num1+num2;
 }
-float division(int num1, int num2) {
+float division(float num1, float num2) {
 	if(num2==0){
 	cout <<"no se puede dividir";
 	} else
 	return num1/num2;
+}
+int main() {
+	float num1, num2;
+	cin >> num1;
+	cin >> num2;
+	cout <<"la suma es: "<<suma(num1, num2)<<endl;
+	cout <<"la division es: "<<division(num1, num2)<<endl;
 }
