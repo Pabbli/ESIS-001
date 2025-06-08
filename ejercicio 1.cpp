7e1bcc95903f4dcab9e3edaa8459e8db21f8f534
pabbli <pfroquem@unjbg.edu.pe>
2025-06-08 14:40:38 -0500

    creacion funcion suma

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -4,4 +4,9 @@ using namespace std;
 float suma(int num1, int num2) {
 	return num1+num2;
 }
-
+float division(int num1, int num2) {
+	if(num2==0){
+	cout <<"no se puede dividir";
+	} else
+	return num1/num2;
+}
