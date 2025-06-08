2a66d5049b6229f50cde695bec3946e6437541f6
pabbli <pfroquem@unjbg.edu.pe>
2025-06-08 15:22:58 -0500

    creacion opcion

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -1,19 +1,33 @@
 //ejercici 1: funcione sumas y division
 #include <iostream>
 using namespace std;
+
 float suma(int num1, int num2) {
 	return num1+num2;
 }
+
 float division(float num1, float num2) {
 	if(num2==0){
 	cout <<"no se puede dividir";
 	} else
 	return num1/num2;
 }
+
 int main() {
 	float num1, num2;
+	int opcion;
+	
+	cout <<"ingrese primer numero: ";
 	cin >> num1;
+	cout <<"ingrese segundo numero: ";
 	cin >> num2;
+	
+	cout << "\nSeleccione la operación a realizar:" << endl;
+	cout <<"1. suma"<<endl;
+	cout <<"2. division"<<endl;
+	cout << "ingrese la opcion: ";
+	cin >> opcion;
+	
 	cout <<"la suma es: "<<suma(num1, num2)<<endl;
 	cout <<"la division es: "<<division(num1, num2)<<endl;
 }
