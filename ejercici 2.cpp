02a0b05b0a1dd0324de186be48eebd406a942f65
pabbli <pfroquem@unjbg.edu.pe>
2025-06-14 18:03:41 -0500

    ejercicio 2 terminado

--- ejercici 2.cpp
+++ ejercici 2.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 using namespace std;
 
-int conciente(int a, int b) {
+int cociente(int a, int b) {
 	int c = 0;
 	while (a >= b) {
 		a = a-b;
@@ -11,10 +11,22 @@ int conciente(int a, int b) {
 	return c;
 }
 
-int reciduo(int a, int b) {
+int residuo(int a, int b) {
 	while (a >= b) {
 		a = a-b;
 	}
 	return a;
+}
+
+int main() {
+	int a, b;
+	cout <<"ingrese numero enteros: ";
+	cin >> a;
+	cout <<"ingrese segundo numero enteros: ";
+	cin >>b;
+	
+	cout <<"cociente es : " <<cociente(a,b)<< endl;
+	cout <<"residuo es : " <<residuo(a,b)<< endl;
+	return 0;
 }
 
