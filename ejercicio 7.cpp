c38fa42e1a6ed0edad30fc13cd750cf08ca458e6
pabbli <pfroquem@unjbg.edu.pe>
2025-06-21 21:07:52 -0500

    ejercicio 7 parte final

--- ejercicio 7.cpp
+++ ejercicio 7.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int factorial(int num) {
 	int resultado = 1;
-	for (int i = 2; i <= num; i++) {
+	for (int i = 1; i <= num; i++) {
 		resultado = resultado * i;
 	}
 	return resultado;
@@ -12,12 +12,12 @@ int combinatorio(int m, int n) {
 	return factorial(m) / (factorial(n) * factorial(m - n));
 }
 int main() {
+	int m,n;
 	cout << "ingrese el valor de m: ";
 	cin >> m;
 	cout << "ingrese el valor de n: ";
 	cin >> n;
 	
-	if (n > m; m < 0; n < 0) {
-		cout << ""
-	}
+		cout << "el numero combinatoro ("<< m <<", "<< n <<") es: "  <<combinatorio(m, n)<< endl;
+		return 0;
 }
